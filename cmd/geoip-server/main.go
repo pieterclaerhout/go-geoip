6a10955da8b1ff11ea8703dc9f0149c3fe51930d
Pieter Claerhout <pieter@yellowduck.be>
2020-05-12 08:42:19 +0200

    Improved the way we read the environment files
    If there is a .env file in the working directory, it will also be loaded.

--- cmd/geoip-server/main.go
+++ cmd/geoip-server/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"github.com/pieterclaerhout/go-log"
-	"github.com/pieterclaerhout/go-webserver"
+	"os"
+	"path/filepath"
 
+	"github.com/joho/godotenv"
 	"github.com/pieterclaerhout/go-geoip/cmd/geoip-server/core"
 	"github.com/pieterclaerhout/go-geoip/cmd/geoip-server/geoip"
+	"github.com/pieterclaerhout/go-log"
+	"github.com/pieterclaerhout/go-webserver"
 )
 
 func main() {
 
 	log.PrintColors = true
 	log.PrintTimestamp = true
+
+	wd, _ := os.Getwd()
+	envPath := filepath.Join(wd, ".env")
+	godotenv.Load(envPath)
 
 	server := webserver.New()
 	server.Register(&core.Core{})
